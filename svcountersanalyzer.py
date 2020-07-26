4a0175710474a73587c525216b95fa0196b6b9a2
Daksh Srivastava <daksh.srivastava.10@gmail.com>
2020-07-26 12:46:25 -0400

    Getting necessary lines from file for 1 file

--- svcountersanalyzer.py
+++ svcountersanalyzer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 from pprint import pprint
-import os
-import sys
+import os, sys, re
 
 def todictionary():
     dct = findEachFile()
@@ -12,9 +11,7 @@ def todictionary():
 
 def findEachFile():
     dct = {}
-    n = ""
     for root, dirs, files in os.walk( '/home/daksh/Projects/svCountersAnalyzer/snapshots', topdown=False):
-        n = root
         for name in files:
             date = os.path.basename(root)
             hour = name.split('_')[1]
@@ -27,13 +24,13 @@ def findEachFile():
     return dct
 
 def findTheWordSVCLI(dct):
-    interesting_line = False
+    interesting_line = False     # Getting variables ready
     section_number = 0
-
+    interesting_lst = list()
     for d,info in dct.items():
         fname = info['path']
         print("PROCESSING FILE:", fname)
-        with open( fname, 'r') as f:
+        with open( fname, 'r') as f:    #Opening files
             for l in f:
                 line = l.strip() # now we have each line
 
@@ -41,20 +38,38 @@ def findTheWordSVCLI(dct):
                 if "SVDIAG" in line and interesting_line:
                     interesting_line = False
                     print("INTERESTING LINES END")
-                    if section_number > 0:
-                        sys.exit(1)
 
-                # Processing intresting lines
+                # Adding interesting line
                 if interesting_line:
-                    print( line)
+                    print(line)
+                    interesting_lst.append(line)       #Adding line
+                    continue
 
-                if "SVCLI" in line and "show interface inspection" in line:
+                if line == "SVDIAG SVCLI: show interface inspection" and "show interface inspection" in line:
                     interesting_line = True
                     section_number = section_number + 1
                     print("INTERESTING LINES START")
                     print( line)
-
-    return ""
+        pprint(interesting_lst)
+        # Finding row values, column values, and sections
+        dct_to_append = dict()
+        """ 
+        Dct_structure : {"SVDIAG 
+        SVCLI: show interface inspection":
+        {<section nname i.e. IP CONTROL PATH>:
+        {<row-column i.e. AdminShunts-Upstream(bytes)>:
+                <value i.e. 0>}}}
+        """
+        #part_indexes = interesting_lst.index("============")
+        
+        #print(part_indexes)
+        #pprint(interesting_lst)
+        st = []
+        for x in interesting_lst:
+            if x not in st:
+                st.append(x)
+        return ""
+        break
 
 if __name__ == "__main__":
     print(todictionary())
